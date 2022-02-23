3d7e4403a89ec6f2a36eb216bdb5f82e39fc6478
Cakesoft-Sameer <sameer@cakesofttech.com>
2022-02-23 13:45:35 +0530

    Function Top

--- src/renderers/ContextMenu.js
+++ src/renderers/ContextMenu.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import { I18nManager, Animated, Easing, StyleSheet, PixelRatio } from 'react-native';
-import { OPEN_ANIM_DURATION, CLOSE_ANIM_DURATION, USE_NATIVE_DRIVER } from '../constants';
+import React from "react";
+import {
+  I18nManager,
+  Animated,
+  Easing,
+  StyleSheet,
+  PixelRatio,
+} from "react-native";
+import {
+  OPEN_ANIM_DURATION,
+  CLOSE_ANIM_DURATION,
+  USE_NATIVE_DRIVER,
+} from "../constants";
 
 const axisPosition = (oDim, wDim, tPos, tDim) => {
   // if options are bigger than window dimension, then render at 0
@@ -16,7 +26,7 @@ const axisPosition = (oDim, wDim, tPos, tDim) => {
     return tPos + tDim - oDim;
   }
   // compute center position
-  let pos = Math.round(tPos + (tDim / 2) - (oDim / 2));
+  let pos = Math.round(tPos + tDim / 2 - oDim / 2);
   // check top boundary
   if (pos < 0) {
     return 0;
@@ -42,6 +52,33 @@ function fit(pos, len, minPos, maxPos) {
   return pos;
 }
 // fits options (position) into safeArea
+
+const axisPositionTop = (oDim, wDim, tPos, tDim) => {
+  // if options are bigger than window dimension, then render at 0
+  if (oDim > wDim) {
+    return 0;
+  }
+  // render at trigger position if possible
+  if (tPos + oDim <= wDim) {
+    return 1;
+  }
+  // aligned to the trigger from the bottom (right)
+  if (tPos + tDim - oDim >= 0) {
+    return 2;
+  }
+  // compute center position
+  let pos = Math.round(tPos + tDim / 2 - oDim / 2);
+  // check top boundary
+  if (pos < 0) {
+    return 3;
+  }
+  // check bottom boundary
+  if (pos + oDim > wDim) {
+    return 4;
+  }
+  // if everything ok, render in center position
+  return 5;
+};
 export const fitPositionIntoSafeArea = (position, layouts) => {
   const { windowLayout, safeAreaLayout, optionsLayout } = layouts;
   if (!safeAreaLayout) {
@@ -52,10 +89,10 @@ export const fitPositionIntoSafeArea = (position, layouts) => {
   const { width: wWidth } = windowLayout;
   let { top, left, right } = position;
   top = fit(top, oHeight, saY, saY + saHeight);
-  left = fit(left, oWidth, saX, saX + saWidth)
-  right = fit(right, oWidth, wWidth - saX - saWidth, saX)
+  left = fit(left, oWidth, saX, saX + saWidth);
+  right = fit(right, oWidth, wWidth - saX - saWidth, saX);
   return { top, left, right };
-}
+};
 
 export const computePosition = (layouts, isRTL) => {
   const { windowLayout, triggerLayout, optionsLayout } = layouts;
@@ -64,7 +101,7 @@ export const computePosition = (layouts, isRTL) => {
   const { height: oHeight, width: oWidth } = optionsLayout;
   const top = axisPosition(oHeight, wHeight, tY - wY, tHeight);
   const left = axisPosition(oWidth, wWidth, tX - wX, tWidth);
-  const start = isRTL ? 'right' : 'left';
+  const start = isRTL ? "right" : "left";
   const position = { top, [start]: left };
   return fitPositionIntoSafeArea(position, layouts);
 };
@@ -73,15 +110,11 @@ export const computeInitial = (layouts, isRTL) => {
   const { x: wX, y: wY, width: wWidth, height: wHeight } = windowLayout;
   const { x: tX, y: tY, height: tHeight, width: tWidth } = triggerLayout;
   const { height: oHeight, width: oWidth } = optionsLayout;
-  const top = axisPosition(oHeight, wHeight, tY - wY, tHeight);
-  const left = axisPosition(oWidth, wWidth, tX - wX, tWidth);
-  const start = isRTL ? 'right' : 'left';
-  const position = { top, [start]: left };
-  return position;
+  const top = axisPositionTop(oHeight, wHeight, tY - wY, tHeight);
+  return top;
 };
 
 export default class ContextMenu extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -99,7 +132,7 @@ export default class ContextMenu extends React.Component {
   }
 
   close() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       Animated.timing(this.state.scaleAnim, {
         duration: CLOSE_ANIM_DURATION,
         toValue: 0,
@@ -112,17 +145,19 @@ export default class ContextMenu extends React.Component {
   render() {
     const { style, children, layouts, ...other } = this.props;
     const animation = {
-      transform: [ { scale: this.state.scaleAnim } ],
+      transform: [{ scale: this.state.scaleAnim }],
       opacity: this.state.scaleAnim,
     };
     const position = computePosition(layouts, I18nManager.isRTL);
     return (
-      <Animated.View {...other} style={[styles.options, style, animation, position]}>
+      <Animated.View
+        {...other}
+        style={[styles.options, style, animation, position]}
+      >
         {children}
       </Animated.View>
     );
   }
-
 }
 
 // public exports
@@ -132,13 +167,13 @@ ContextMenu.computeInitial = computeInitial;
 
 export const styles = StyleSheet.create({
   options: {
-    position: 'absolute',
+    position: "absolute",
     borderRadius: 2,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     width: PixelRatio.roundToNearestPixel(200),
 
     // Shadow only works on iOS.
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.3,
     shadowOffset: { width: 3, height: 3 },
     shadowRadius: 4,
